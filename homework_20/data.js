ef9671cb10f1bc1433243cacf011cee0538e2b30
Resgul <60659541+Resgul@users.noreply.github.com>
2020-09-30 18:42:02 +0300

    Add files via upload

--- homework_20/data.js
+++ homework_20/data.js
@@ -1,34 +1,48 @@
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCQ7h4ZPAUvujRy0AUHU7NlE0IjDzXTa4g",
-//   authDomain: "project-resgul.firebaseapp.com",
-//   databaseURL: "https://project-resgul.firebaseio.com",
-//   projectId: "project-resgul",
-//   storageBucket: "project-resgul.appspot.com",
-//   messagingSenderId: "1006199687151",
-//   appId: "1:1006199687151:web:c1fa2c1830fdba44ad581a",
-//   measurementId: "G-X8KJS47900"
-// };
+var firebaseConfig = {
+  apiKey: "AIzaSyCQ7h4ZPAUvujRy0AUHU7NlE0IjDzXTa4g",
+  authDomain: "project-resgul.firebaseapp.com",
+  databaseURL: "https://project-resgul.firebaseio.com",
+  projectId: "project-resgul",
+  storageBucket: "project-resgul.appspot.com",
+  messagingSenderId: "1006199687151",
+  appId: "1:1006199687151:web:c1fa2c1830fdba44ad581a",
+  measurementId: "G-X8KJS47900"
+};
 
-// firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
-// const firebase = require("firebase");
-// require("firebase/firestore");
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
 
-// var db = firebase.firestore();
-// //Добавление и запись
-// db.collection("Reshad").add({
-//   name: "Reshad",
-//   score: 45
-// })
-// .then(function(docRef) {
-//   console.log("Document written with ID: ", docRef.id);
-// })
-// .catch(function(error) {
-//   console.error("Error adding document: ", error);
-// });
-// //Получение
-// db.collection("Reshad").get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         console.log(`${doc.id} => ${doc.data()}`);
-//     });
-// });
+function writeResult(obj, base) {
+  base.collection("starship_game").add({
+  playerName: obj.playerName = (obj.playerName == '') ? 'player' : obj.playerName,
+  score: obj.score
+  })
+}
+
+function sortResultsMaxToMin(arrObj) {
+  arrObj.sort(function (a, b) {
+    return b.score - a.score;
+  })
+}
+
+function createScroresList(uList, arr) {
+  for (let i = 0; i < 15; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${i+1}. ${arr[i].name} --- score: ${arr[i].score}`;
+    uList.append(li);
+  }
+  return uList
+}
+
+function showResults(results, area, base) {
+  results.classList.remove('hide');
+  let ul = document.querySelector('.ul-results');
+  let responseArray = [];
+  base.collection('starship_game').get().then(response => response.forEach(list => {  
+    responseArray.push({name: list.data().playerName, score: list.data().score});
+    sortResultsMaxToMin(responseArray);
+  })).then(() => {
+    createScroresList(ul, responseArray);
+    area.append(ul);
+  })
+}
--- homework_20/script.js
+++ homework_20/script.js
@@ -2,20 +2,39 @@ let starship = document.createElement('div'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea'),
   score = document.querySelector('.score'),
-  bang = document.createElement('div');
+  bang = document.createElement('div'),
+  fire = document.createElement('div'),
+  time = document.querySelector('.timer'),
+  player = document.querySelector('.player-name'),
+  results = document.querySelector('.results');
 starship.classList.add('starship');
 bang.classList.add('bang');
+fire.classList.add('fire');
 
 let settings = {
+  playerName: player.value,
+  timeout: 20,
   score: 0,
   x1: 0,
   x2: 0,
   y: 0
 }
 
+function timer(obj, ship) {
+  time.textContent = `time: ${obj.timeout}`;
+  obj.timeout--;
+  if (obj.timeout >= 0) {
+    return setTimeout(() => timer(obj, ship), 1000)
+  } else {
+    writeResult(settings, db);
+    showResults(results, gameArea, db);
+    ship.remove();
+  }
+}
+
 function startButton() {
   start.classList.toggle('hide');
-  startGame();
+  startGame(starship);
 }
 
 function shipMoving(y, ship) {
@@ -52,18 +71,47 @@ function scoreUP(score, obj) {
 }
 
 function getDamages(obj, ship) {
-  if (obj.score === 2) ship.style.background = 'url(/img/ship/starship1.png)';
+  if (obj.score === 2) {
+  ship.style.background = 'url(img/ship/starship1.png)';
   ship.style.backgroundSize = 'contain';
-  if (obj.score === 5) ship.style.background = 'url(/img/ship/starship2.png)';
-  ship.style.backgroundSize = 'contain';
-  if (obj.score === 6) ship.style.background = 'url(/img/bang.gif)';
-  ship.style.backgroundSize = 'contain';
-  ship.style.backgroundRepeat = 'no-repeat';
+  }
+  if (obj.score === 4) {
+    ship.style.background = 'url(img/ship/starship2.png)';
+    ship.style.backgroundSize = 'contain';
+  }
+  if (obj.score === 8) {
+    ship.style.background = 'url(img/ship/starship3.png)';
+    ship.style.backgroundSize = 'contain';
+  }
+  if (obj.score === 12) {
+    ship.style.background = 'url(img/ship/starship4.png)';
+    ship.style.backgroundSize = 'contain';
+  }
+  if (obj.score === 18) {
+    ship.style.background = 'url(img/ship/starship5.png)';
+    ship.style.backgroundSize = 'contain';
+  }
+  if (obj.score === 20) {
+    ship.style.background = 'url(img/bang.gif)';
+    ship.style.backgroundSize = 'contain';
+    ship.style.width = '400px';
+    ship.style.height = '400px';
+    ship.style.backgroundRepeat = 'no-repeat';
+  }
 }
 
-function startGame() {
-  gameArea.appendChild(starship);
-  starship.addEventListener('mouseover', () => {
+function shipCreation(area ,ship) {
+  ship.style.left = '35%';
+  ship.style.top = '45%';
+  return area.appendChild(ship)
+}
+
+function startGame(ship) {
+  player.classList.add('hide');
+  settings.playerName = player.value;
+  timer(settings, starship);
+  shipCreation(gameArea, starship);
+  ship.addEventListener('mouseover', () => {
     setTimeout(() => {
       shooting(starship, bang);
       shipMoving(settings.y, starship);
